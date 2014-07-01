3969d7b8be413d0a01c9a371db7b9845ae77ede2
Robert Beisicht <jokelab@hotmail.com>
2014-07-01 15:54:39 +0200

    Add demo files

---
+++ Ui.js
@@ -0,0 +1,116 @@
+﻿
+$(document).ready(function () {
+     context = document.getElementById("canvas").getContext("2d");
+
+    //create outline context
+     outlineContext = document.getElementById("outlines").getContext("2d");
+    outlineContext.fillStyle = "red";
+
+    $('#canvas').mousedown(function (e) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+
+        paint = true;
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        redraw();
+    });
+
+    $('#canvas').mousemove(function (e) {
+        if (paint) {
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        }
+    });
+
+
+    $('#canvas').mouseup(function (e) {
+        paint = false;
+    });
+
+    $('#canvas').mouseleave(function (e) {
+        paint = false;
+    });
+
+    $("#clear").on("click", function () {
+        //clear drawing context
+        clickX = [];
+        clickY = [];
+        clickDrag = [];
+        paint = false;
+        redraw();
+
+        //clear outline context
+        outlineContext.clearRect(0, 0, outlineContext.canvas.width, outlineContext.canvas.height);
+
+    });
+        
+    $("#read").on("click", readDrawing);
+
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+    var paint;
+
+    function addClick(x, y, dragging) {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+    }
+
+    function redraw() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+        context.strokeStyle = "green";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for (var i = 0; i < clickX.length; i++) {
+            context.beginPath();
+            if (clickDrag[i] && i) {
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+            } else {
+                context.moveTo(clickX[i] - 1, clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    }
+
+
+}
+);
+
+//Create and return an array with 225 items (15x15) which represents a downsampled image
+function getDownsampledDrawing() {
+    var output = [];
+    var blockSize = 10;
+    var canvasSizeX = context.canvas.width;
+    var canvasSizeY = context.canvas.height;
+
+    for (var x = 0; x < canvasSizeX; x += blockSize) {
+        for (var y = 0; y < canvasSizeY; y += blockSize) {
+            //get the RGBA value per pixel
+            var data = context.getImageData(x, y, blockSize, blockSize).data;
+            var found = false;
+            for (var i = 0; i < data.length; i++) {
+                //if at least one of the values is greater than 0, the square is hit by the user
+                if (data[i]) {
+                    output.push(1);
+
+                    //draw red square on the outline context
+                    outlineContext.fillRect(x, y, blockSize, blockSize);
+
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                output.push(0);
+            }
+
+        }
+    }
+
+    return output;
+}
